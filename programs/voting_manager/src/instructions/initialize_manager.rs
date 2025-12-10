080e1d2cc9870d5c94ed5e8c363852285ae994bd
MateuszWisniewskii <mdw180703@gmail.com>
2025-12-10 20:35:20 +0100

    uzupełnienie kontekstu i handlera initialize_manager. stworzenie konta manager

--- programs/voting_manager/src/instructions/initialize_manager.rs
+++ programs/voting_manager/src/instructions/initialize_manager.rs
@@ -1,15 +1,31 @@
 use anchor_lang::prelude::*;
+use voting::ANCHOR_DISCRIMINATOR_SIZE;
+
+use crate::Manager;
 
 #[derive(Accounts)]
 pub struct InitializeManager<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
 
+    #[account(
+        init,
+        payer = authority,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Manager::INIT_SPACE,
+        seeds = [b"manager_seed".as_ref(), authority.key().as_ref()],
+        bump
+    )]
+    pub manager: Account<'info, Manager>,
+
     #[account()]
     pub system_program: Program<'info, System>,
 }
 
 pub fn handler(ctx: Context<InitializeManager>) -> Result<()> {
+    let manager = &mut ctx.accounts.manager;
 
+    manager.authority = ctx.accounts.authority.key();
+    manager.poll_count = 0;
+    
     Ok(())
 }
